8990e13f07dc35ed713b9444e4a29eea6514ca82
Samarth Gupta <samarthgupta1202@gmail.com>
2022-05-15 16:44:10 +0530

    files updated

--- buffer.java
+++ buffer.java
@@ -8,8 +8,11 @@ public class buffer {
         String name=br.readLine();
         System.out.println("enter phone number:");
         int phone=Integer.parseInt(br.readLine());
+        System.out.println("enter marks:");
+        float marks=Float.parseFloat(br.readLine());
         System.out.println("Your name is "+name);
         System.out.println("phone is "+phone);
+        System.out.println("marks are "+marks);
 
     }
 }

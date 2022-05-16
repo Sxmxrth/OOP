ec2fb18769f3032a5765dcdbf72258884c37b0c3
Samarth Gupta <samarthgupta1202@gmail.com>
2022-05-16 09:58:16 +0530

    packages added

--- buffer.java
+++ buffer.java
@@ -4,7 +4,7 @@ public class buffer {
     public static void main(String[] args) throws IOException, NumberFormatException {
 
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        
+
         System.out.println("enter name:");
         String name=br.readLine();
 
---
+++ packages/a/greeting.java
@@ -0,0 +1,7 @@
+package packages.a;
+
+public class greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
